4dc9d5551ae72628294711c9aec7045a07a16779
Krishjain21 <112113439+Krishjain21@users.noreply.github.com>
2023-03-28 06:20:32 +0530

    Add files via upload
    final completed website for stage 1

---
+++ captcha/index.js
@@ -0,0 +1,40 @@
+//
+//
+// document.getElementById("sname1").innerHTML = "New text!";
+// document.getElementById("simg1").innerHTML = "New text!";
+// document.getElementById("course1").innerHTML = "New text!";
+// document.getElementById("teacher1").innerHTML = "New text!";
+// document.getElementById("time1").innerHTML = "New text!";
+// document.getElementById("rating1").innerHTML = "New text!";
+//
+//
+// document.getElementById("sname2").innerHTML = "New text!";
+// document.getElementById("simg2").innerHTML = "New text!";
+// document.getElementById("course2").innerHTML = "New text!";
+// document.getElementById("teacher2").innerHTML = "New text!";
+// document.getElementById("time2").innerHTML = "New text!";
+// document.getElementById("rating2").innerHTML = "New text!";
+//
+//
+// document.getElementById("sname3").innerHTML = "New text!";
+// document.getElementById("simg3").innerHTML = "New text!";
+// document.getElementById("course3").innerHTML = "New text!";
+// document.getElementById("teacher3").innerHTML = "New text!";
+// document.getElementById("time3").innerHTML = "New text!";
+// document.getElementById("rating3").innerHTML = "New text!";
+//
+//
+// document.getElementById("sname4").innerHTML = "New text!";
+// document.getElementById("simg4").innerHTML = "New text!";
+// document.getElementById("course4").innerHTML = "New text!";
+// document.getElementById("teacher4").innerHTML = "New text!";
+// document.getElementById("time4").innerHTML = "New text!";
+// document.getElementById("rating4").innerHTML = "New text!";
+//
+//
+// document.getElementById("sname5").innerHTML = "New text!";
+// document.getElementById("simg5").innerHTML = "New text!";
+// document.getElementById("course5").innerHTML = "New text!";
+// document.getElementById("teacher5").innerHTML = "New text!";
+// document.getElementById("time5").innerHTML = "New text!";
+// document.getElementById("rating5").innerHTML = "New text!";
